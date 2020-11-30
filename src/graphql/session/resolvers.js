77c35f4ac255b137be5ded78fa94f1cfe85d3c37
Alec <alec.paglia@gmail.com>
2020-11-29 19:38:11 -0500

    First working version of the sessions notes and attendance

--- src/graphql/session/resolvers.js
+++ src/graphql/session/resolvers.js
@@ -1,6 +1,6 @@
 import { ForbiddenError } from "apollo-server-express";
-import { Session } from "./../../models";
 import { ObjectDoesNotExistError } from "./../errors/ObjectDoesNotExistError";
+import { Session, User } from "./../../models";
 
 const resolvers = {
   Query: {
@@ -13,17 +13,17 @@ const resolvers = {
     editAttendance: async (_, { sessionId, studentId, status }, context) => {
       const allowedRoles = ["ROLE_TUTOR", "ROLE_ADMIN"];
       // Auth
-      if (!context.isAuth || allowedRoles.indexOf(context.role) === -1) throw new ForbiddenError();
+      if (!context.auth.isAuth || allowedRoles.indexOf(context.auth.role) === -1) throw new ForbiddenError();
 
       // Objects exist
       const [se, stu] = await Promise.all([
-        Session.findById(sessionId).select("_id", "tutor").populate("tutor").exec(),
+        Session.findById(sessionId).select(["_id", "tutor"]).populate("tutor").exec(),
         User.findById(studentId).select("_id").lean().exec(),
       ]);
       if (!se || !stu) throw new ObjectDoesNotExistError();
 
       // Is the current logged user (Tutor) the person assigned to that session
-      if (se.tutor.id !== context.userId) throw new ForbiddenError();
+      if (se.tutor.id !== context.auth.userId) throw new ForbiddenError();
 
       // Updates
       return Session.findOneAndUpdate(
@@ -37,22 +37,23 @@ const resolvers = {
         }
       )
         .populate("tutor")
-        .populate("attendance.student");
+        .populate("attendance.student")
+        .exec();
     },
     editNotes: async (_, { sessionId, notes }, context) => {
       const allowedRoles = ["ROLE_TUTOR", "ROLE_ADMIN"];
       // Auth
-      if (!context.isAuth || allowedRoles.indexOf(context.role) === -1) throw new ForbiddenError();
+      if (!context.auth.isAuth || allowedRoles.indexOf(context.auth.role) === -1) throw new ForbiddenError();
 
       // Objects exist
-      const se = await Session.findById(sessionId).select("_id", "tutor").populate("tutor").exec();
+      const se = await Session.findById(sessionId).select(["_id", "tutor"]).populate("tutor").exec();
       if (!se) throw new ObjectDoesNotExistError();
 
       // Is the current logged user (Tutor) the person assigned to that session
-      if (se.tutor.id !== context.userId) throw new ForbiddenError();
+      if (se.tutor.id !== context.auth.userId) throw new ForbiddenError();
 
       // Updates
-      Session.findOneAndUpdate(
+      return Session.findOneAndUpdate(
         { _id: sessionId },
         {
           $set: { notes: notes },
